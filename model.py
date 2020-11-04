e1b8a4eddc7589116f079cc441ace5fe960c690e
Natasha Anderson <andersonnatashaa@gmail.com>
2020-11-04 06:32:18 +0000

    add user_id Column

--- model.py
+++ model.py
@@ -8,3 +8,8 @@ class User(db.Model):
     """A user."""
 
     __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True
+                        )

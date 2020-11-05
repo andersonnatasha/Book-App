003b1fdb2aa30c1a1c748c655341faa5f1837905
Natasha Anderson <andersonnatashaa@gmail.com>
2020-11-05 22:54:16 +0000

    create create_user()

--- crud.py
+++ crud.py
@@ -3,8 +3,23 @@
 from model import db, User, Book, BookCopy, Author, Category, BookCategory, connect_to_db
 
 
+def create_user(email, password, full_name=None, gender=None, created_at):
+    """Create and return a new user."""
+
+    user = User(email=email,
+                password=password
+                full_name=full_name,
+                gender=gender,
+                created_at=created_at)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
 
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
-    
+
+

012dea150e7444cdef92a054eda508b820b168bd
Natasha Anderson <andersonnatashaa@gmail.com>
2020-11-06 00:49:32 +0000

    create 10 test users; import Faker for fake data

--- seed_database.py
+++ seed_database.py
@@ -3,6 +3,7 @@
 import os 
 import json 
 from datetime import datetime
+from faker import Faker
 
 import crud
 import model
@@ -13,3 +14,15 @@ os.system('createdb bookslibrary')
 
 model.connect_to_db(server.app)
 model.db.create_all()
+
+fake = Faker()
+
+# Create 10 random users with unique emails
+for n in range(10):
+    email = f'user{n}@test.com'
+    password = f'testpassword{n}'
+    full_name = fake.name()
+    birthday = datetime.strptime(fake.date(), '%Y-%m-%d')
+    created_at = datetime.strptime(fake.date(), '%Y-%m-%d')
+
+    crud.create_user(email, password, full_name, birthday, created_at)

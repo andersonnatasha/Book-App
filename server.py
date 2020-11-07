bad7a10bbc8ac8f8a10a062d0b5b8b8de5c2c733
Natasha Anderson <andersonnatashaa@gmail.com>
2020-11-07 02:38:53 +0000

    work on register()

--- server.py
+++ server.py
@@ -6,9 +6,10 @@ import crud
 from jinja2 import StrictUndefined
 
 import os
+from datetime import datetime
 
 app = Flask(__name__)
-app.secrect_key = os.environ['FLASK_KEY']
+app.secret_key = os.environ['FLASK_KEY']
 
 
 @app.route('/')
@@ -17,11 +18,45 @@ def homepage():
 
     return render_template('homepage.html')
 
+
 @app.route('/sign-up')
 def sign_up():
-    """Register a user"""
+    """View sign up page"""
 
     return render_template('sign_up.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    """Register a user"""
+
+    email = request.form.get('email')
+    email_confirmed = request.form.get('confirm-email')
+    password = request.form.get('password')
+    password_confirmed = request.form.get('confirm-password')
+    profile_name = request.form.get('profile-name')
+    birth_month = request.form.get('birth-month')
+    birth_day = request.form.get('birth-day')
+    birth_year = request.form.get('birth-year')
+    gender = request.form.get('gender')
+
+    time_created = datetime.now()
+
+    birthday = f'{birth_year}/{birth_month}/{birth_day}'
+
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash('Account already exists')
+    elif email != email_confirmed:
+        flash('emails do not match')
+    elif password != password_confirmed:
+        flash('passwords do not match')
+    else:
+        crud.create_user(email, password, profile_name, birthday, gender, time_created)
+
+    return redirect("/login-in)
 
 @app.route('/log-in')
 def login():

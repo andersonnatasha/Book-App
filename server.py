b9301fc16294a8b36fbcde1a3d794699452a23a1
Natasha Anderson <andersonnatashaa@gmail.com>
2020-11-15 06:36:56 +0000

    read/liked/tbr buttons fully work

--- server.py
+++ server.py
@@ -282,11 +282,15 @@ def search_a_book():
             search_result = {}
 
             base = data['items'][n]['volumeInfo']
+            search_result['isbn_13'] = None
 
             # industry_identifiers = base.get('industryIdentifiers', None) and
-            if base.get('industryIdentifiers', None) and ((base['industryIdentifiers'][-1]['type'] == 'ISBN_13') or (base['industryIdentifiers'][0]['type'] == 'ISBN_13'))  :
-                search_result['isbn_13'] = base['industryIdentifiers'][1]['identifier']
+            if base.get('industryIdentifiers', None) and (base['industryIdentifiers'][-1]['type'] == 'ISBN_13'):
+                search_result['isbn_13'] = base['industryIdentifiers'][-1]['identifier']
+            elif base.get('industryIdentifiers', None) and (base['industryIdentifiers'][0]['type'] == 'ISBN_13')
+                search_result['isbn_13'] = base['industryIdentifiers'][-1]['identifier']
 
+            if search_result['isbn_13'] != None
                 title = base['title']
                 search_result['title'] = title
 

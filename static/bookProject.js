2df1faeb6fe366a1209fd6fad107507eb0a3cbb6
Natasha Anderson <andersonnatashaa@gmail.com>
2020-11-20 03:24:56 +0000

    working on putting new bookshelf to end

--- static/bookProject.js
+++ static/bookProject.js
@@ -14,6 +14,24 @@
 
 
 
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modal-content');
+const newBookshelfButton = document.getElementById('create-bookshelf-button');
+const cancelButton = document.getElementById('cancel');
+
+newBookshelfButton.addEventListener('click', function() {
+  modalContent.style.display = 'flex'
+  modal.style.display ='block'
+  console.log('open')
+});
+
+cancelButton.addEventListener('click', function() {
+  modalContent.style.display = 'none'
+  modal.style.display ='none'
+  console.log('closed')
+});
+
+
 
 $('#create-bookshelf').on('submit', (evt) => {
     evt.preventDefault();
@@ -24,6 +42,8 @@ $('#create-bookshelf').on('submit', (evt) => {
 
     $.post('/create-bookshelf.json', formInput, (res) => {
         const newDiv = $('#bottom-of-bookshelf').append(res.name);
+        modalContent.style.display = 'none'
+        modal.style.display ='none'
         // const newH3 = $newDiv.append('h3');
         // newH3.after(res.name)
         });
@@ -32,60 +52,5 @@ $('#create-bookshelf').on('submit', (evt) => {
 
 
 
-  const modal = document.getElementById('modal');
-  const modalContent = document.getElementById('modal-content');
-  const newBookshelfButton = document.getElementById('create-bookshelf-button');
-  const cancelButton = document.getElementById('cancel');
-
-  newBookshelfButton.addEventListener('click', function() {
-    modalContent.style.display = 'flex'
-    modal.style.display ='block'
-    console.log('open')
-  });
-
-  cancelButton.addEventListener('click', function() {
-    modalContent.style.display = 'none'
-    modal.style.display ='none'
-    console.log('closed')
-  });
-  console.log(modal)
 
 
-
-// document.querySelector('#create-bookshelf-button').addEventListener('click', (event) => {
-//    console.log('hi');
-// }
-
-// $('#create-bookshelf-button').on('click', (evt) => {
-
-//   $('#create-bookshelf-modal').showModal()
-
-//   const formInput = {
-//   melonWaffles: $('#bookshelf-name').val()
-//   };
-
-//   $.post('/create-bookshelf.json', formInput, (res) => {
-//       const newDiv = $('#bottom-of-bookshelf').append(res.name);
-//       // const newH3 = $newDiv.append('h3');
-//       // newH3.after(res.name)
-//       });
-// });
-
-
-
-
-// $.get('https://pokeapi.co/api/v2/berry/', (res) => {
-//   const berryNames = [];
-//   for (const berry of res.results) {
-//     berryNames.push(berry.name);
-//   }
-
-//   $('#berries').append(berryNames.join(', '));
-// });
-
-
-/// could loop on 36, and for each thing looped over could append or make children to the H3 div.
-
-
-/// could prepopulate the page.
-///Could load stuff into HTML page.
